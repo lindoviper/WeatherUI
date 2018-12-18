ba4280d0bc724f94bb47431fa2ff85549b9283a8
lindoviper <Lindo.Mbuthuma@gmail.com>
2018-12-18 17:50:34 +0200

    populate html page with city content

--- Weather/wwwroot/js/site.js
+++ Weather/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$(document).ready(function(){
+    $("select.citylist").change(function(){
+        var selectedValue = $(this).children("option:selected").val();
+        var cityName = document.getElementById('city');
+        cityName.innerText = selectedValue;
+
+    });
+});
